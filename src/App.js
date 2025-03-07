ed32d9b5de621046fc81de81874f436739fb4aea
Elias Nogueira <elias.silva@orion.ufal.br>
2025-03-07 19:18:01 -0300

    add checkbox

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ import "leaflet.markercluster/dist/MarkerCluster.css";
 import "leaflet.markercluster/dist/MarkerCluster.Default.css";
 
 const alunoIcon = new L.Icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/149/149072.png",
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/5850/5850276.png",
+  // iconUrl: "https://cdn-icons-png.flaticon.com/512/149/149072.png",
   iconSize: [30, 30],
   iconAnchor: [15, 30],
   popupAnchor: [0, -25],
@@ -23,7 +24,7 @@ const hospedagemIcon = new L.Icon({
 });
 
 const defaultIcon = new L.Icon({
-  iconUrl: "https://cdn-icons-png.flaticon.com/512/252/252025.png",
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/7051/7051035.png",
   iconSize: [30, 30],
   iconAnchor: [15, 30],
   popupAnchor: [0, -25],
@@ -46,9 +47,12 @@ const HeatmapLayer = ({ points }) => {
   return null;
 };
 
-  const MapaComOpcoes = () => {
+const MapaComOpcoes = () => {
   const [dados, setDados] = useState([]);
   const [modoVisualizacao, setModoVisualizacao] = useState("marcadores");
+  const [mostrarCadUnico, setMostrarCadUnico] = useState(true);
+  const [mostrarAlunos, setMostrarAlunos] = useState(true);
+  const [mostrarHospedagens, setMostrarHospedagens] = useState(true);
   const mapRef = useRef(null);
   const markerClusterRef = useRef(null);
 
@@ -59,17 +63,11 @@ const HeatmapLayer = ({ points }) => {
         const response2 = await axios.get("/enderecos_alunos.json");
         const response3 = await axios.get("/enderecos_hospedagem.json");
   
-        console.log("enderecos.json:", response1.data);
-        console.log("enderecos_alunos.json:", response2.data);
-        console.log("enderecos_hospedagem.json:", response3.data);
-  
         const dados1 = Array.isArray(response1.data) ? response1.data : [];
         const dados2 = Array.isArray(response2.data) ? response2.data : [];
         const dados3 = Array.isArray(response3.data) ? response3.data : [];
   
         const dadosComCoords = [...dados1, ...dados2, ...dados3].filter((item) => item.coordenadas);
-  
-        console.log("Dados combinados:", dadosComCoords);
   
         setDados(dadosComCoords);
       } catch (error) {
@@ -85,8 +83,16 @@ const HeatmapLayer = ({ points }) => {
       markerClusterRef.current = L.markerClusterGroup();
 
       dados.forEach((item) => {
-        const icon = item.tipo === "aluno" ? alunoIcon : item.tipo === "hospedagem" ? hospedagemIcon : defaultIcon;
+        let icon = alunoIcon;
+
+        if (item.nome) {
+          icon = hospedagemIcon; // Diferencia as hospedagens
+        } else if (item.code) {
+          icon = defaultIcon; // Diferencia os beneficiarios
+        }
+
         const marker = L.marker([item.coordenadas.lat, item.coordenadas.lon], { icon });
+
         marker.bindPopup(`<strong>${item.endereco}</strong>`);
         markerClusterRef.current.addLayer(marker);
       });
@@ -102,14 +108,52 @@ const HeatmapLayer = ({ points }) => {
   }, [dados, modoVisualizacao]);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", height: "100vh" }}>
-      <div style={{ margin: "10px" }}>
-        <button onClick={() => setModoVisualizacao("marcadores")} style={buttonStyle}>Marcadores</button>
-        <button onClick={() => setModoVisualizacao("heatmap")} style={buttonStyle}>Heatmap</button>
-        <button onClick={() => setModoVisualizacao("cluster")} style={buttonStyle}>Cluster</button>
+    <div style={{ display: "flex", flexDirection: "row", height: "100vh" }}>
+      <div style={{ width: "200px", padding: "20px", background: "#f4f4f4", borderRadius: "8px" }}>
+        <h4>Bases de Dados</h4>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={mostrarCadUnico}
+              onChange={() => setMostrarCadUnico(!mostrarCadUnico)}
+            />
+            Beneficiários CRIA
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={mostrarAlunos}
+              onChange={() => setMostrarAlunos(!mostrarAlunos)}
+            />
+            Alunos do Oxetech
+          </label>
+        </div>
+        <div>
+          <label>
+            <input
+              type="checkbox"
+              checked={mostrarHospedagens}
+              onChange={() => setMostrarHospedagens(!mostrarHospedagens)}
+            />
+            Hospedagens SETUR
+          </label>
+        </div>
+        <h4>Modos de exibição</h4>
+        <div>
+          <button onClick={() => setModoVisualizacao("marcadores")} style={buttonStyle}>Marcadores</button>
+        </div>
+        <div>
+          <button onClick={() => setModoVisualizacao("heatmap")} style={buttonStyle}>Heatmap</button>
+        </div>
+        <div>
+          <button onClick={() => setModoVisualizacao("cluster")} style={buttonStyle}>Cluster</button>
+        </div>
       </div>
 
-      <div style={{ width: "80vw", height: "80vh", borderRadius: "10px", overflow: "hidden", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)" }}>
+      <div style={{ width: "80vw", height: "100vh", borderRadius: "10px", overflow: "hidden", boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)" }}>
         <MapContainer
           center={[-9.5713, -36.7819]}
           zoom={10}
@@ -119,23 +163,25 @@ const HeatmapLayer = ({ points }) => {
           <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
 
           {modoVisualizacao === "marcadores" &&
-            dados.map((item, index) => {
-              let icon = defaultIcon;
-
-              if (item.nome) {
-                icon = hospedagemIcon; // Diferencia as hospedagens
-              } else if (item.code) {
-                icon = alunoIcon; // Diferencia as pessoas no cad
-              }
-              else {
-                icon = defaultIcon; // Diferencia os alunos
-              }
-              return (
-                <Marker key={index} position={[item.coordenadas.lat, item.coordenadas.lon]} icon={icon}>
-                  <Popup>{item.endereco} {item.nome ? `(${item.nome})` : ""}</Popup>
-                </Marker>
-              );
-            })
+            dados
+              .filter(item => 
+                (mostrarAlunos && !item.code && !item.nome) || // Mostrar CAD ÚNICO
+                (mostrarCadUnico && item.code) || // Mostrar Alunos do Oxetech
+                (mostrarHospedagens && item.nome) // Mostrar Hospedagens
+              )
+              .map((item, index) => {
+                let icon = alunoIcon;
+                if (item.nome) {
+                  icon = hospedagemIcon;
+                } else if (item.code) {
+                  icon = defaultIcon;
+                }
+                return (
+                  <Marker key={index} position={[item.coordenadas.lat, item.coordenadas.lon]} icon={icon}>
+                    <Popup>{item.endereco} {item.nome ? `(${item.nome})` : ""}</Popup>
+                  </Marker>
+                );
+              })
           }
 
           {modoVisualizacao === "heatmap" && (
